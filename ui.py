51764f6d557ff59b6c6e12fcb76f0e3f8f96fc8e
Sam Jackson <samahstan@gmail.com>
2015-07-25 09:29:19 -0500

    Added the hander to ui.py

--- ui.py
+++ ui.py
@@ -1,8 +1,13 @@
 from gi.repository import Gtk
 
+class Handler:
+    def onDeleteWindow(self, *args):
+        Gtk.main_quit(*args)
+
 def windowStart():
     builder = Gtk.Builder()
     builder.add_from_file("interface.glade")
+    builder.connect_signals(Handler())
+
     mainWindow = builder.get_object("window1")
     mainWindow.show_all()
-    mainWindow.connect("delete-event", Gtk.main_quit)
